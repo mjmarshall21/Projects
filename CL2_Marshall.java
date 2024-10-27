95c551b8d5079d225cf1554e2674194e9faf9d9a
Matthew Marshall <mjmarshall021@gmail.com>
2024-10-26 18:41:35 -0600

    Update CL2_Marshall.java

--- CL2_Marshall.java
+++ CL2_Marshall.java
@@ -1,15 +1,7 @@
 import java.util.Scanner;
 import java.io.*;
 
-/* Matthew Marshall
-[CS1101] Comprehensive Lab 2
-This work is to be done individually. It is not permitted to. share, reproduce, 
-or alter any part of this assignment for any purpose. Students are not permitted 
-to share code, upload this assignment online in any form, or view/receive/ modifying 
-code written by anyone else. This assignment is part. of an academic course at The 
-University of Texas at El Paso and a grade will be assigned for the work produced 
-individually by the student.
-*/
+
 public class CL2_Marshall{
 	static char[][] maze; // maze variable 
 	static int[] startPos = new int[2]; // Start position set
